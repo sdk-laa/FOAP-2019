f39b2c618c9e90a757c16ce124819b768fe6913b
foap <foap@BI101-15.epsevg.upc.es>
2019-04-25 13:17:01 +0200

    a

--- P2/Ejercicio1.5.final.php
+++ P2/Ejercicio1.5.final.php
@@ -13,12 +13,13 @@
                 //print_r(substr($Palabras[0],0,2));  "para coger las dos primeras letras de la palabra
                 //echo "Hay ".str_word_count($String, 0). " palabras en la cadena '$String'";
                 $encadenada=true;
+
                 for ($i=0; $i<count($Palabras)-1; $i++){
                     if ((substr($Palabras[$i],-2)) != (substr($Palabras[$i+1],0,2))){
                         print("<br>las palabras de la cadena no estan encadenadas". "\n");
                         $encadenada=false;
                     }
-                    else ($encadenda){
+                    if ($encadenda){
                         echo "<br>";
                         echo ($Palabras[$i]);
                         echo "<br>";
