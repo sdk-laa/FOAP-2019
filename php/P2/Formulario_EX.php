2bc7c3d606b8863751ca82a7fcd2d96635628add
sdk-laa <sdk.laaroussi@gmail.com>
2019-05-03 16:08:07 +0200

    Registro

--- php/P2/Formulario_EX.php
+++ php/P2/Formulario_EX.php
@@ -83,9 +83,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <!--<style>
-            .error {color: #FF0000;}
-        </style> -->
+
     <link rel="stylesheet" type="text/css" href="../../CSS/estiloT.css" />
     <title>Pagina de noticias PHP</title>
     
