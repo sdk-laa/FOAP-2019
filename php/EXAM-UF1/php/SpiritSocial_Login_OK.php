4fe20a896dd3ae2a6cef25fb53cd11b9a89f4bd9
sdk-laa <sdk.laaroussi@gmail.com>
2019-05-10 16:14:49 +0200

    a

--- php/EXAM-UF1/php/SpiritSocial_Login_OK.php
+++ php/EXAM-UF1/php/SpiritSocial_Login_OK.php
@@ -57,6 +57,26 @@
                     <br><br>
                     <br><br>
                     <div>
+                        <b><p><span class="titulo"> Luis Suárez se pierde la final de Copa</span></p></b>
+                        <p><span> El club azulgrana emitió un comunicado en el que aseguraba que la intervención había sido un éxito</span></p>
+                        <p><span> y que el uruguayo no podrá estar en Sevilla.</span></p>      
+                        <p><img src= "../imgs/Suarez.jpg" alt="Logo" height="400px" width="700px"></p>   
+                        <input type="submit" name="Like" value="Like">
+                        <p><span class="titulo"> Comment:</span></p>  
+                        <textarea name="comment" rows="5" cols="40" <?php echo $comment;?>></textarea>
+                        <br><br>
+                    </div>
+                    <div>
+                        <b><p><span class="titulo"> El Barça acelera los contactos finales por De Ligt</span></p></b>
+                        <p><span> Tras el KO del Ajax en la Champions, el Barça quiere dejarlo todo lista para abordar</span></p>
+                        <p><span>su contratación cuando termine la Eredivise.</span></p>      
+                        <p><img src= "../imgs/DeLigt.jpg" alt="Logo" height="400px" width="700px"></p> 
+                        <input type="submit" name="Like" value="Like">
+                        <p><span class="titulo"> Comment:</span></p>  
+                        <textarea name="comment" rows="5" cols="40" <?php echo $comment;?>></textarea>
+                        <br><br>
+                    </div>
+                    <div>
                         <b><p><span class="titulo"> Griezmann rumbo a Barcelona:</span></p></b>
                         <p><span> El francés concita el quorum en el área técnica del Barça por su calidad, experiencia y precio.</span></p>      
                         <p><span> La debacle en Champions refuerza la idea de poner competencia y fichar gol en la delantera.</span></p> 
@@ -67,24 +87,7 @@
                         <br><br>
                         <br><br>
                     </div>
-                    <div>
-                        <b><p><span class="titulo"> Luis Suárez se pierde la final de Copa</span></p></b>
-                        <p><span> El club azulgrana emitió un comunicado en el que aseguraba que la intervención había sido un éxito y que el uruguayo no podrá estar en Sevilla.</span></p>      
-                        <p><img src= "../imgs/Suarez.jpg" alt="Logo" height="400px" width="700px"></p>   
-                        <input type="submit" name="Like" value="Like">
-                        <p><span class="titulo"> Comment:</span></p>  
-                        <textarea name="comment" rows="5" cols="40" <?php echo $comment;?>></textarea>
-                        <br><br>
-                    </div>
-                    <div>
-                        <b><p><span class="titulo"> El Barça acelera los contactos finales por De Ligt</span></p></b>
-                        <p><span> Tras el KO del Ajax en la Champions, el Barça quiere dejarlo todo lista para abordar su contratación cuando termine la Eredivise.</span></p>      
-                        <p><img src= "../imgs/DeLigt.jpg" alt="Logo" height="400px" width="700px"></p> 
-                        <input type="submit" name="Like" value="Like">
-                        <p><span class="titulo"> Comment:</span></p>  
-                        <textarea name="comment" rows="5" cols="40" <?php echo $comment;?>></textarea>
-                        <br><br>
-                    </div>
+
 
                 </form>
                 <!--<div style="float:left"> <img src= "../imgs/redesS.png" alt="Logo" height="750px" width="400px"></div>-->
