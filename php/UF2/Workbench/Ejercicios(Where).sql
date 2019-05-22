6876ac5f5662ac7146810d3d6931d58cee9ea304
foap <foap@BI101-15.epsevg.upc.es>
2019-05-22 13:56:51 +0200

    Where

---
+++ php/UF2/Workbench/Ejercicios(Where).sql
@@ -0,0 +1,13 @@
+SELECT DISTINCT oficina from empleats;
+SELECT nom, oficina, edat from empleats where oficina = 12 and edat>30;
+SELECT numemp, nom from empleats where vendes>salari;
+SELECT numemp, nom from empleats where contracte<"1988-01-01";
+SELECT numemp, nom, YEAR(contracte) from empleats where YEAR(contracte)<1988;
+SELECT * from oficines where vendes<(objectiu*0.8);
+SELECT * from oficines where dir=108;
+SELECT * from empleats where vendes between 100000 and 500000;
+SELECT * from empleats where vendes>=100000 and vendes<=500000;
+SELECT numemp, nom from empleats where oficina=12 or oficina=14 or oficina=16;
+  
+
+
