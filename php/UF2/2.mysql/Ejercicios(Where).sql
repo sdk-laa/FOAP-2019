1cf72cc531761b2f204b4b56b334a1b2a2eff14b
foap <foap@BI101-15.epsevg.upc.es>
2019-05-23 13:53:39 +0200

    SQL TOTAL

--- php/UF2/2.mysql/Ejercicios(Where).sql
+++ php/UF2/2.mysql/Ejercicios(Where).sql
@@ -8,3 +8,12 @@ SELECT * from oficines where dir=108;
 SELECT * from empleats where vendes between 100000 and 500000;
 SELECT * from empleats where vendes>=100000 and vendes<=500000; 
 SELECT numemp, nom from empleats where oficina=12 or oficina=14 or oficina=16;
+SELECT numemp, nom from empleats where oficina in(12,14,16);
+SELECT * from oficines where dir is null;
+SELECT * from empleats where oficina is not  null;
+SELECT * from empleats where nom like  "Luis%";
+SELECT * from empleats where nom like  "%Luis%";
+SELECT * from empleats where nom like  "__a%";
+SELECT preu, preu*1.21 from productes;
+SELECT numcomanda, codfab, codprod, quant, import, quant*import, 
+
--- php/UF2/Workbench/Ejercicios(Where).sql
+++ php/UF2/Workbench/Ejercicios(Where).sql
@@ -8,6 +8,9 @@ SELECT * from oficines where dir=108;
 SELECT * from empleats where vendes between 100000 and 500000;
 SELECT * from empleats where vendes>=100000 and vendes<=500000;
 SELECT numemp, nom from empleats where oficina=12 or oficina=14 or oficina=16;
+SELECT numemp, nom from empleats where oficina like 12 or oficina like 14 or oficina like 16;
+SELECT * from oficines where dir like "NULL";
+SELECT * from empleats where oficina NOT LIKE "NULL";
   
 
 
